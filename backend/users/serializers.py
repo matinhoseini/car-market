ef738fb6298155ae9f11ca10822c0d73fe94c49c
Matin Hoseini <matin.hs.ir@gmail.com>
2026-07-06 19:14:14 +0330

    add authentication (register + jwt login)

---
+++ backend/users/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
+
+    
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords do not match")
+
+        return data
+
+   
+    def validate_username(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Username must be at least 3 characters")
+
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username already exists")
+
+        return value
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+
+        return user
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(["POST"])
+def register(request):
+    username = request.data["username"]
+    password = request.data["password"]
+
+    user = User.objects.create_user(
+        username=username,
+        password=password
+    )
+
+    return Response({
+        "message": "user created",
+        "user": user.username
+    })
