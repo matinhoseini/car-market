d68d6a6c88f43b1a21494e6bf3c0fcfd56d74fe3
Matin Hoseini <matin.hs.ir@gmail.com>
2026-07-20 21:32:53 +0330

    feat(chat): add websocket chat foundation

---
+++ backend/chat/jwt_middleware.py
@@ -0,0 +1,44 @@
+from urllib.parse import parse_qs
+
+from channels.middleware import BaseMiddleware
+from channels.db import database_sync_to_async
+
+from rest_framework_simplejwt.tokens import AccessToken
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@database_sync_to_async
+def get_user(user_id):
+    try:
+        return User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return AnonymousUser()
+
+
+class JwtAuthMiddleware(BaseMiddleware):
+
+    async def __call__(self, scope, receive, send):
+
+        scope["user"] = AnonymousUser()
+
+        query_string = parse_qs(scope["query_string"].decode())
+
+        token = query_string.get("token")
+
+        if token:
+            try:
+                access_token = AccessToken(token[0])
+
+                user = await get_user(access_token["user_id"])
+
+                scope["user"] = user
+
+                print("USER =", user)
+
+            except Exception as e:
+                print("JWT ERROR =", e)
+
+        return await super().__call__(scope, receive, send)
